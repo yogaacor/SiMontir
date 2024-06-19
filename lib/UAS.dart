7d4e8b0ea9929932dc73df171bb6fab2b8a562db
Yoga Yudha Tama <yogayudhatama48@gmail.com>
2024-06-19 11:17:33 +0700

    UAS RPL 2

--- lib/UAS.dart
+++ lib/UAS.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class UAS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
